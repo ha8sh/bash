7ec91c752e6615aa46705e7460d1b982a605c2ce
ha8sh <73406593+ha8sh@users.noreply.github.com>
2020-12-06 21:41:16 +0330

    Update Auto_reboot.sh

--- Auto_reboot.sh
+++ Auto_reboot.sh
@@ -4,4 +4,6 @@ ln -s /usr/share/zoneinfo/Asia/Tehran /etc/localtime
 cat << EOF > /var/spool/cron/root
 30 5 * * * root certbot renew --quiet && systemctl restart ocserv >/dev/null 2>&1
 EOF
+echo "Please Wait"
+sleep 3
 echo "Done"
