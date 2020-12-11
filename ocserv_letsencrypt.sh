858b17dd513722437f9d8ab40705ece687124237
ha8sh <73406593+ha8sh@users.noreply.github.com>
2020-12-12 01:05:49 +0330

    Create ocserv_letsencrypt.sh

---
+++ ocserv_letsencrypt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -r -p "Enter your Mail: " MAIL
+read -r -p "Enter your Domain: " DOMAIN
+
+certbot certonly --standalone --preferred-challenges http --agree-tos --email $EMAIL -d $DOMAIN
+
+sed -i -e 's/'server-cert'/'#server-cert'/g' /etc/ocserv/ocserv.conf
+sed -i -e 's/'server-key'/'#server-key'/g' /etc/ocserv/ocserv.conf
+
+cat << EOF >> /etc/ocserv/ocserv.conf
+server-cert = /etc/letsencrypt/live/$DOMAIN/fullchain.pem
+server-key = /etc/letsencrypt/live/$DOMAIN/privkey.pem
+EOF
+
+systemctl restart ocserv
