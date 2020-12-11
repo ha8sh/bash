8ae9f4d4df5de5b3540485bd135d8231ca6dd0fa
ha8sh <73406593+ha8sh@users.noreply.github.com>
2020-12-12 01:07:18 +0330

    Update ocserv_letsencrypt.sh

--- ocserv_letsencrypt.sh
+++ ocserv_letsencrypt.sh
@@ -2,7 +2,7 @@
 read -r -p "Enter your Mail: " MAIL
 read -r -p "Enter your Domain: " DOMAIN
 
-certbot certonly --standalone --preferred-challenges http --agree-tos --email $EMAIL -d $DOMAIN
+certbot certonly --standalone --preferred-challenges http --agree-tos --email $MAIL -d $DOMAIN
 
 sed -i -e 's/'server-cert'/'#server-cert'/g' /etc/ocserv/ocserv.conf
 sed -i -e 's/'server-key'/'#server-key'/g' /etc/ocserv/ocserv.conf
