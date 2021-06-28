59d84dd5e5e141a1185b64975773555182219d2b
ha8sh <73406593+ha8sh@users.noreply.github.com>
2021-06-28 20:23:42 +0430

    Update fix_centos6_yum.sh

--- fix_centos6_yum.sh
+++ fix_centos6_yum.sh
@@ -5,6 +5,6 @@ mv *.repo OldRepo
 wget https://git.io/JIGuO -O CentOS-Vault.repo
 yum clean all
 yum remove epel-release -y
-wget /tmp https://vault.centos.org/6.10/extras/x86_64/Packages/epel-release-6-8.noarch.rpm
+wget -O /tmp/epel-release-6-8.noarch.rpm https://vault.centos.org/6.10/extras/x86_64/Packages/epel-release-6-8.noarch.rpm
 rpm -Uvh /tmp/epel-release-6-8.noarch.rpm
 yum update
